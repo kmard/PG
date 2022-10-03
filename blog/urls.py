f538916e28f4c4bd5d6df8cf8912999355dd955e
KM <tsnok777@gmail.com>
2022-10-03 12:42:29 +0300

    connect temlates to main page

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
+# app_name = 'blog'
 
 urlpatterns = [
     # path('january/', views.january,name='january'),
     # path('february/', views.february,name='february'),
     path('', views.starting_page, name='starting_page'),
-    path('posts',views.posts,name='post-page' ),
-    path('posts/<slug>',views.post_detail,name='post-detail-page' ),
+    path('posts/',views.posts,name='post-page' ),
+    path('posts/<slug:slug>',views.post_detail,name='post-detail-page' ),
 
 ]

ef9a984f00cf0739817670af7ef7c244481e3322
KM <tsnok777@gmail.com>
2022-11-02 16:10:55 +0200

    app blog Outputting Comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-
+from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 class StartingPageView(ListView):
     template_name = 'blog/index.html'
@@ -32,19 +36,35 @@ class AllPostsView(ListView):
 #     posts = Post.objects.all().order_by('-date')
 #     return render(request, 'blog/all-posts.html',{'all_posts':posts})
 
-class SinglePostView(DetailView):
-    template_name = 'blog/post-detail.html'
-    model = Post
+class SinglePostView(View):
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(** kwargs)
-        context['post_tags'] = self.object.tags.all()
-        context['comment_form'] = CommentForm()
-        return context
+    def get(self,request,slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post':post,
+            'post_tags':post.tags.all(),
+            'comment_form':CommentForm(),
+            'comments':post.comments.all().order_by('-id'),
+        }
+        return render(request, 'blog/post-detail.html',context)
+
+    def post(self,request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post-detail-page',
+                                                args=[slug]))
+
+        context = {
+            'post':post,
+            'post_tags':post.tags.all(),
+            'comment_form':CommentForm,
+            'comments': post.comments.all().order_by('-id'),
+        }
+        return render(request, 'blog/post-detail.html', context)
 
 
-# def post_detail(request,slug):
-#     identified_post = get_object_or_404(Post,slug=slug)
-#     return render(request, 'blog/post-detail.html',{'post':identified_post,
-#                                                     'post_tags':identified_post.tags.all()})
 
